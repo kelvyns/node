8c19774cbc4858ba86c8d89fc368c631b7dd080a
Kelvyns Aguilar <kelvynsaguilar@gmail.com>
2015-07-02 18:08:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ var express    = require("express");
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var team = require('./routes/team');
-
+var config = require('./lib/config');
+var mock = require('./routes/index');
+var updater = require('./routes/updater');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', routes);
-app.use('/team', team);
-
-
+app.use('/', mock);
+app.use('/api/updater/', updater);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,5 +45,4 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000, '127.0.0.1', function(){ console.log('server listening on port 3000') });
-//app.listen(3000, '10.181.4.89', function(){ console.log('server listening on port 3000') });
+app.listen(config.local_port, config.local_ip, function(){ console.log('server listening on port '+ config.local_port ); });
